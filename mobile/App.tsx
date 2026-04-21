e7ec659579560761db801539d8b161023cabc2b9
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2026-04-21 16:30:21 +0800

    initializing mobile app

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textContainer}>Welcome to Aera!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    fontSize: 30,
+    fontWeight: 900,
+    textAlign: "center"
+  }
+});
