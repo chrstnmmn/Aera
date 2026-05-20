8baa896f11d4ffa9ab96fe66b2acc572a49b14c3
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2026-05-20 15:32:53 +0800

    developing the masterboard and connecting to the router and wifi connection

--- mobile/src/components/ui/ControlCenter.tsx
+++ mobile/src/components/ui/ControlCenter.tsx
@@ -7,7 +7,7 @@ import BookmarkIcon from "../icons/BookmarkIcon";
 
 const { width } = Dimensions.get("window");
 
-const CIRCLE_SIZE = 38;
+const CIRCLE_SIZE = 38;w
 
 interface Props {
   theme: any;
